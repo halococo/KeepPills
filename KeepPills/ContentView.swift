3df48b6d0bcbd7618a7134dd1c9feab5d411c771
Byul Kang <halococoa@gmail.com>
2023-11-24 16:03:27 +0900

    * Add readme with Tech Stack. * Label for the main screen.

--- KeepPills/ContentView.swift
+++ KeepPills/ContentView.swift
@@ -11,12 +11,34 @@ import SwiftData
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [Item]
-
+    
     @State private var todayDate = Date()
-
+    
     var body: some View {
-        Text(formatDate(date:todayDate))
+        /*
+         Text(formatDate(date:todayDate)).onAppear{
+         self.startTimer()
+         }
+         */
+        Label(
+            title: {
+                Text("약스케쥴")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(.gray)
+            },
+            icon: {
+                Image(systemName: "heart")
+                    .font(.largeTitle)
+                    .foregroundColor(.red)
+            })
     }
+
+/*
+    func startTimer() {
+        Timer.scheduledTimer(withTimeInterval: 60, repeats: true) { _ in self.todayDate = Date()}
+    }
+*/
   
     func formatDate(date: Date) -> String {
         let formatter = DateFormatter()
---
+++ Readme.txt
@@ -0,0 +1,13 @@
+This project's Tech Stack includes:
+
+Development Environment: Xcode 15.0.1
+Continuous Integration and Delivery: Xcode Cloud
+Operating System: iOS SDK 17.0
+Database Management: SwiftDB
+Cloud Storage and Syncing: CloudKit
+User Interface Framework: SwiftUI
+Design and Prototyping: Figma
+Iconography: SF Symbols
+Code Collaboration and Version Control: GitHub
+
+Github : 
